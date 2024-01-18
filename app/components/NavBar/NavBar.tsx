68fa91d53f12ad69189f455ff1954bd06bf1a202
DenisOuma <dennisouma38@gmail.com>
2024-01-18 03:57:45 +0300

    Setting up the mobile Ui view design and page break for the desktop

---
+++ app/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const NavBar = () => {
+	return (
+		<nav id="navbar">
+			<div className="navbar-content">
+				{/* give me a good classname for the logo */}
+				<div className="logo">Logo</div>
+				{/* give me a good classname for the menu */}
+				<div className="menu">Menu</div>
+			</div>
+		</nav>
+	);
+};
